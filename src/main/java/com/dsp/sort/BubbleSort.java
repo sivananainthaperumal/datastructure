037f44453ff0171b7fb3d5648b36cfca7c1e5901
sivananainthaperumal <sivananaintha.perumal@gmail.com>
2023-10-08 13:06:52 -0400

    bubble sort and selection sort

---
+++ src/main/java/com/dsp/sort/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.dsp.sort;
+
+import java.util.Arrays;
+
+/*
+ 1. Iterate decrement of j from n to o.
+ 2. Iterate increment of i to j
+ 3. Every iteration will leave the last element sorted.
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int array[] = {2,5,23,1,6,3,67,9,32};
+
+        BubbleSort bs = new BubbleSort();
+        bs.print(array);
+        bs.sort(array);
+        bs.print(array);
+    }
+
+    private void sort(int[] array) {
+        System.out.println("Bubble Sort");
+        int n= array.length-1;
+        System.out.println(n);
+        for (int j = n; j >0 ; j--) {
+            for (int i = 0; i < j; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+    }
+
+    private void print(int[] array){
+        Arrays.stream(array).forEach(val->System.out.print(val+" "));
+    }
+}
---
+++ src/main/java/com/dsp/sort/SelectionSort.java
@@ -0,0 +1,46 @@
+package com.dsp.sort;
+
+import java.util.Arrays;
+
+/*
+1. double iterating the array.
+2. first iteration the first the starting index
+3. Second iteration starts after the sorted index
+4. After every iteration left idx is sorted
+5. For every iteration swap the index(i) with the min of the rest of the array.
+ */
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int array[] = {2,5,23,1,6,3,67,9,32};
+        SelectionSort ss = new SelectionSort();
+        ss.print(array);
+        ss.sort(array);
+        ss.print(array);
+    }
+
+    private void sort(int[] array) {
+
+        int n= array.length;
+        for (int i = 0; i < n; i++) {
+            int minIdx = i;
+            for (int j = i; j < n; j++) {
+                if(array[minIdx]>array[j]){
+                    minIdx=j;
+                }
+            }
+            int temp = array[minIdx];
+            array[minIdx]=array[i];
+            array[i]=temp;
+
+        }
+    }
+
+
+    private void print(int[] array) {
+        Arrays.stream(array).forEach(val-> System.out.print(val+" "));
+        System.out.println();
+    }
+
+}
